c18b2f14958c8ee683eaebadf22989c11e441e21
shiki.takahashi <shiki.takahashi@linecorp.com>
2020-04-30 18:22:03 +0900

    fix c03 s2

--- chapter03_entity/src/s2.rs
+++ chapter03_entity/src/s2.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use derive_getters::Getters;
 use anyhow::Result;
 
@@ -5,39 +7,52 @@ use common::MyError;
 
 #[derive(Clone, Debug, Getters)]
 pub struct User {
-    name: String,
+    name: Name,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Name(String);
+
 impl User {
-    pub fn new(name: &str) -> Result<Self> {
-        if name.chars().count() < 3 {
-            bail!(MyError::type_error("User's name should be grater than 3"));
-        }
-        Ok(Self { name: name.to_string() })
+    pub fn new(name: Name) -> Self {
+        Self { name }
     }
 
-    pub fn change_name(&mut self, name: Option<&str>) -> Result<()> {
+    pub fn change_name(&mut self, name: Name) -> Result<()> {
+        self.name = name;
+        Ok(())
+    }
+}
+
+impl Name {
+    fn new(name: Option<&str>) -> Result<Self>  {
         match name {
             Some(name) => {
                 match name {
-                    name if name.chars().count() > 3 => self.name = name.to_string(),
+                    name if name.chars().count() > 3 => Ok(Name(name.to_string())),
                     _ => bail!(MyError::type_error("User's name should be grater than 3")),
                 }
             }
             None => bail!(MyError::none_error("User's name is nil")),
-        };
-
-        Ok(())
+        }
     }
 }
 
 #[test]
 fn test_change_name() {
-    let mut u1 = User::new("taro").unwrap();
-    assert_eq!("taro".to_string(), u1.name);
+    let name1 = Name::new(Some("taro")).unwrap();
+    let name1_cloned = name1.clone();
 
-    u1.change_name(Some("jiro"));
-    assert_eq!("jiro".to_string(), u1.name);
+    let mut u1 = User::new(name1);
 
-    assert!(u1.change_name(None).is_err());
+    assert_eq!(name1_cloned, u1.name);
+
+    let name2 = Name::new(Some("jiro")).unwrap();
+    let name2_cloned = name2.clone();
+    u1.change_name(name2);
+
+    assert_eq!(name2_cloned, u1.name);
+
+    assert!(Name::new(None).is_err());
+    assert!(Name::new(Some("a")).is_err());
 }
